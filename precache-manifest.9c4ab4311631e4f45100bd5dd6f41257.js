d343e8e2a566a4fbde0eeac51b89bb701de28fc2
moneerAlDakkak <moner.dakkak.5@gmail.com>
2021-10-10 17:05:44 +0300

    deploy

---
+++ precache-manifest.9c4ab4311631e4f45100bd5dd6f41257.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d348c7440a991a386d39",
+    "url": "/Neptone/css/app.abc9fd9d.css"
+  },
+  {
+    "revision": "3f58250de519643ba770",
+    "url": "/Neptone/css/chunk-1bcb8c2c.28403633.css"
+  },
+  {
+    "revision": "96fad4cb6473b8fcc8e2",
+    "url": "/Neptone/css/chunk-26ececcb.fe59508f.css"
+  },
+  {
+    "revision": "01557bf78f1dd9f9a4f7",
+    "url": "/Neptone/css/chunk-36bb47c4.c29ff4d4.css"
+  },
+  {
+    "revision": "92efc419eb0c73b9d092",
+    "url": "/Neptone/css/chunk-588aa41e.1952fafd.css"
+  },
+  {
+    "revision": "8ca04994d44fa329faa9",
+    "url": "/Neptone/css/chunk-7b7c0cb8.4dea0105.css"
+  },
+  {
+    "revision": "9109744481f1fe5f5cf0",
+    "url": "/Neptone/css/chunk-7c6c1cb2.edd64146.css"
+  },
+  {
+    "revision": "b1dc0c6758e8c333c805",
+    "url": "/Neptone/css/chunk-aa8e1d80.609c2e28.css"
+  },
+  {
+    "revision": "060f9a2bc246f77c7d643e6ab2349034",
+    "url": "/Neptone/img/female.060f9a2b.png"
+  },
+  {
+    "revision": "ba3e7bdc5a2186760b8f60796258e17d",
+    "url": "/Neptone/img/male.ba3e7bdc.png"
+  },
+  {
+    "revision": "30a851675b6a02cc361b6445b1ebcc8f",
+    "url": "/Neptone/img/my-ad.30a85167.jpg"
+  },
+  {
+    "revision": "eb98a23572e49d147c59b9357ad94c61",
+    "url": "/Neptone/img/weDontDoThatHere.eb98a235.jpg"
+  },
+  {
+    "revision": "e0b354483be24d0ef765ba18bbc4b648",
+    "url": "/Neptone/index.html"
+  },
+  {
+    "revision": "d348c7440a991a386d39",
+    "url": "/Neptone/js/app.9b7b2df4.js"
+  },
+  {
+    "revision": "abc476f417f5f6d33187",
+    "url": "/Neptone/js/chunk-058c73ff.34af7f06.js"
+  },
+  {
+    "revision": "3f58250de519643ba770",
+    "url": "/Neptone/js/chunk-1bcb8c2c.b4cd023d.js"
+  },
+  {
+    "revision": "96fad4cb6473b8fcc8e2",
+    "url": "/Neptone/js/chunk-26ececcb.03aa9902.js"
+  },
+  {
+    "revision": "01557bf78f1dd9f9a4f7",
+    "url": "/Neptone/js/chunk-36bb47c4.968e106c.js"
+  },
+  {
+    "revision": "92efc419eb0c73b9d092",
+    "url": "/Neptone/js/chunk-588aa41e.b4896b0b.js"
+  },
+  {
+    "revision": "8ca04994d44fa329faa9",
+    "url": "/Neptone/js/chunk-7b7c0cb8.4dd4bce2.js"
+  },
+  {
+    "revision": "9109744481f1fe5f5cf0",
+    "url": "/Neptone/js/chunk-7c6c1cb2.6873841e.js"
+  },
+  {
+    "revision": "b1dc0c6758e8c333c805",
+    "url": "/Neptone/js/chunk-aa8e1d80.b324a7bf.js"
+  },
+  {
+    "revision": "c3da15817be9c99b39e8",
+    "url": "/Neptone/js/chunk-vendors.fcc4b0c9.js"
+  },
+  {
+    "revision": "cbf63a656bfd9f622033783f3e342cb8",
+    "url": "/Neptone/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Neptone/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Neptone/precache-manifest.9c4ab4311631e4f45100bd5dd6f41257.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "neptone"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
